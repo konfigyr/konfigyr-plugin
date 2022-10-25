8f1848d0edac2c1d6991ceb6ed92f6c440980a87
Vladimir Spasic <vladimir.spasic.86@gmail.com>
2022-10-25 23:27:11 +0200

    Fix tests, drop Java 17 as a build target

--- konfigyr-plugin-core/src/test/java/com/konfigyr/HttpClientConfigurationMetadataUploaderTest.java
+++ konfigyr-plugin-core/src/test/java/com/konfigyr/HttpClientConfigurationMetadataUploaderTest.java
@@ -1,15 +1,18 @@
 package com.konfigyr;
 
+import com.github.tomakehurst.wiremock.http.RequestMethod;
 import com.github.tomakehurst.wiremock.junit5.WireMockRuntimeInfo;
 import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 import com.github.tomakehurst.wiremock.matching.RequestPatternBuilder;
 import com.konfigyr.test.TestFactories;
+import org.apache.hc.core5.http.HttpHeaders;
 import org.apache.hc.core5.http.HttpStatus;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.util.List;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -22,6 +25,11 @@ class HttpClientConfigurationMetadataUploaderTest {
 
     @Test
     void shouldUploadArtifactMetadata(WireMockRuntimeInfo wireMock) throws Exception {
+        registerMappingFor(wireMock, "/upload/com.acme/acme/1.0.0");
+        registerMappingFor(wireMock, "/upload/com.acme/acme/1.0.1");
+        registerMappingFor(wireMock, "/upload/com.acme/core/1.0.2");
+        registerMappingFor(wireMock, "/upload/com.acme/spring/1.0.2");
+
         final ConfigurationMetadataUploader uploader = create(wireMock, "test-access-token");
         final List<ConfigurationMetadata> metadata = uploader.upload();
 
@@ -39,10 +47,25 @@ class HttpClientConfigurationMetadataUploaderTest {
                 );
 
         wireMock.getWireMock().verifyThat(4, RequestPatternBuilder.allRequests());
+        verifyMappingFor("/upload/com.acme/acme/1.0.0");
+        verifyMappingFor("/upload/com.acme/acme/1.0.1");
+        verifyMappingFor("/upload/com.acme/core/1.0.2");
+        verifyMappingFor("/upload/com.acme/spring/1.0.2");
     }
 
     @Test
     void shouldFailDueToAuthenticationException(WireMockRuntimeInfo wireMock) throws Exception {
+        wireMock.getWireMock().register(
+                post(anyUrl())
+                        .withHeader(HttpHeaders.AUTHORIZATION, equalToIgnoreCase("Bearer invalid-access-token"))
+                        .withHost(equalToIgnoreCase("localhost"))
+                        .withPort(wireMock.getHttpPort())
+                        .withScheme("http")
+                        .willReturn(
+                                jsonResponse("{}", HttpStatus.SC_UNAUTHORIZED)
+                        )
+        );
+
         final ConfigurationMetadataUploader uploader = create(wireMock, "invalid-access-token");
 
         assertThatThrownBy(uploader::upload)
@@ -55,6 +78,17 @@ class HttpClientConfigurationMetadataUploaderTest {
 
     @Test
     void shouldFailDueToPermissionException(WireMockRuntimeInfo wireMock) throws Exception {
+        wireMock.getWireMock().register(
+                post(anyUrl())
+                        .withHeader(HttpHeaders.AUTHORIZATION, equalToIgnoreCase("Bearer forbidden-access-token"))
+                        .withHost(equalToIgnoreCase("localhost"))
+                        .withPort(wireMock.getHttpPort())
+                        .withScheme("http")
+                        .willReturn(
+                                jsonResponse("{}", HttpStatus.SC_FORBIDDEN)
+                        )
+        );
+
         final ConfigurationMetadataUploader uploader = create(wireMock, "forbidden-access-token");
 
         assertThatThrownBy(uploader::upload)
@@ -86,5 +120,24 @@ class HttpClientConfigurationMetadataUploaderTest {
                 .host(info.getHttpBaseUrl())
                 .token(token)
                 .build();
+    }
+
+
+
+    private static void registerMappingFor(WireMockRuntimeInfo info, String path) {
+        info.getWireMock().register(
+                post(urlPathEqualTo(path))
+                        .withHeader(HttpHeaders.AUTHORIZATION, equalToIgnoreCase("Bearer test-access-token"))
+                        .withHost(equalToIgnoreCase("localhost"))
+                        .withPort(info.getHttpPort())
+                        .withScheme("http")
+                        .willReturn(
+                                okJson("{}").withUniformRandomDelay(200, 600)
+                        )
+        );
+    }
+
+    private static void verifyMappingFor(String path) {
+        verify(RequestPatternBuilder.newRequestPattern(RequestMethod.POST, urlPathEqualTo(path)));
     }
 }
