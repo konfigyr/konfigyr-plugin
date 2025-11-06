6078c39ba254d962eaa765b3c48390dc0436241c
Vladimir Spasic <vladimir.spasic.86@gmail.com>
2025-11-06 18:17:16 +0100

    Use `com.konfigyr:konfigyr-artifactory:1.0.0-RC2`

--- konfigyr-plugin-core/build.gradle.kts
+++ konfigyr-plugin-core/build.gradle.kts
@@ -1,5 +1,5 @@
 dependencies {
-    api("com.konfigyr:konfigyr-artifactory:1.0.0")
+    api("com.konfigyr:konfigyr-artifactory:1.0.0-RC2")
     api("tools.jackson.core:jackson-databind:3.0.1")
     api("org.springframework:spring-core:6.2.12")
     api("org.springframework.boot:spring-boot-configuration-metadata:3.5.7")
